4ee53d12c85954dec90aff0c8cfcaa27e79a8eff
heeseon <hisundl@naver.com>
2024-03-11 16:20:13 +0900

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,7 @@
 public class Main {
     // main 입력하고 탭
     public static void main(String[] args) {
-        //sysout 아니라 sout
-        //sout 탭
-        System.out.println("HI");
-        System.out.println("HI");
-        System.out.println("HI");
-        System.out.println("HI");
         System.out.println("HI");
     }
+
 }
