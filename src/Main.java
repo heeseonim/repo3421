b45682b344caef4eabfed4146dca3436103494be
heeseon <hisundl@naver.com>
2024-03-12 15:12:05 +0900

    Marin class

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,22 @@
 import java.util.ArrayList;
 
 public class Main {
+    public static class Marin {
+        int hp = 100;
+        void run() {
+            hp -= 10;
+            System.out.println("RUN " + hp);
+        }
+    }
     // main 입력하고 탭
+
     public static void main(String[] args) {
-
+        Marin m1 = new Marin();
+        System.out.println(m1.hp);
+        m1.run();
+        m1.run();
+        m1.run();
+        m1.run();
+        m1.run();
     }
-
-    static void setRemainDays(ArrayList<Integer> roomRemainDays) {
-        for (int num = 0; num < roomRemainDays.size(); num++) {
-            if (roomRemainDays.get(num) == 0) continue;
-            roomRemainDays.set(num, roomRemainDays.get(num) - 1);
-        }
-    }
-
-    class Point {
-        int x;
-        int y;
-
-        void print() {
-            System.out.println(x + " " + y);
-        }
-    }
-
-    class drawMachine {
-        ArrayList<Point> points = new ArrayList<>();
-
-        void addPoint(Point p) {
-            points.add(p);
-        }
-
-        void drawPoints() {
-            for (Point p : points) {
-                p.print();
-            }
-        }
-    }
-
 }
